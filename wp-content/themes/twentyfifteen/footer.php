55e334958229c0c466907b5e61c90921ed054e7f
Yoshi Ishigami <yoshi@ibf.sc>
2015-09-14 15:45:32 +0900

    /index.php add news fun

--- wp-content/themes/twentyfifteen/footer.php
+++ wp-content/themes/twentyfifteen/footer.php
@@ -10,14 +10,15 @@
  */
 ?>
 
-	</div><!-- .site-content -->
-
+  <?php 
+    $page_id = 2;
+    $page = get_post( $page_id );
+    echo $page->post_content;
+  ?>
+  
   <footer>
     <p><img src="/_asset/img/copyrights.png" height="10" width="191" alt="&copy; 2015 DRONE SOLUTIONS inc."></p>
   </footer>
- 
-
-<?php wp_footer(); ?>
-
+  
 </body>
 </html>
--- wp-content/themes/twentyfifteen/index.php
+++ wp-content/themes/twentyfifteen/index.php
@@ -100,9 +100,23 @@ get_header(); ?>
           <h3>News</h3>
           <div class="cont_body">
             <ul class="list_news">
-              <li><a href="#">2015年10月31日<br>ドローン操縦者の派遣サービスを開始しました</a></li>
-              <li><a href="#">2015年9月11日<br>空撮サービスを</a></li>
-              <li><a href="#">2015年9月1日<br>ウェブサイトを公開しました</a></li>
+            <?php if ( have_posts() ) : ?>
+            <?php
+            // Start the loop.
+            while ( have_posts() ) : the_post();
+              if ( is_single() ) :
+              the_title( sprintf( '<li><a href="%s">', esc_url( twentyfifteen_get_link_url() ) ), '</a></li>' );
+              else :
+              the_title( sprintf( '<li><a href="%s">', esc_url( twentyfifteen_get_link_url() ) ), '</a></li>' );
+              endif;
+            // End the loop.
+            endwhile;
+            // If no content, include the "No posts found" template.
+            else :
+              //get_template_part( 'content', 'none' );
+              <li>ただいま投稿されているお知らせはございません</li>
+            endif;
+            ?>
             </ul>
           </div>
       </div>
@@ -131,46 +145,5 @@ get_header(); ?>
     </script>
 
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
-			<?php
-			// Start the loop.
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
-
-			// End the loop.
-			endwhile;
-
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-				'next_text'          => __( 'Next page', 'twentyfifteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
-			) );
-
-		// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
 
 <?php get_footer(); ?>
